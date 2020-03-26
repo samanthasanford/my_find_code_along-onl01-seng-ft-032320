5291b00a36eed8038eebabc89063768cf47b2d77
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 15:34:25 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,18 +1,3 @@
 require 'pry'
 
-def my_find(collection)
-  i = 0 
-  while i < collection.length 
-  i += 1 
-end
-end
-
-def my_find(collection)
-  i = 0 
-  
-  while i < collection.length 
-  yield(collection[i])
-  i += 1 
-end
-end
 
